5481b27c86c882ee14d945a84a0ca8e076b62379
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-07 10:56:56 -0400

    Pass array tests

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.splice(-1);
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    return [...cats,name];
+}
+
+function prependCat(name){
+    return [name,...cats];
+}
+
+function removeLastCat(){
+    return cats.slice(0,-1);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
